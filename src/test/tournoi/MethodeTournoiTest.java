218095fea1905e9e5382c616d89f49a50ff67782
Lucas Martineau <lucasmartineau@yahoo.fr>
2017-01-23 11:39:42 +0100

    Test methodeTournoi

---
+++ src/test/tournoi/MethodeTournoiTest.java
@@ -0,0 +1,17 @@
+package test.tournoi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/** Classe servant à tester les méthodes de MethodeTournoi
+ *  @author Cyril Dernoncourt, Antoine Drouard, Léa Le Bert, Lucas Martineau
+ *  Created by Lucas Martineau on 23/01/17.
+ */
+public class MethodeTournoiTest {
+    @Test
+    public void creerPaire() throws Exception {
+        
+    }
+
+}
